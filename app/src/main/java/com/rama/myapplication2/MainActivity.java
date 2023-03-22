2b56bf602b846ef51f3001ba416e7ecb2dadcf3b
ramadhannezz <ramadhan8100@gmail.com>
2023-03-22 11:02:12 +0700

    Update Design : - Konektivitas login dengan server firebase - Update UI halaman RegisterActivity dan activity_register.xml

--- app/src/main/java/com/rama/myapplication2/MainActivity.java
+++ app/src/main/java/com/rama/myapplication2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.rama.myapplication2;
 
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,68 +8,100 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
-    private EditText UsernameEditText, PasswordEditText;
-    private Button LoginButton, RegisterButton;
+    private EditText EmailEditText, PasswordEditText;
+    private FirebaseAuth mAuth;
+    String email, password;
+    TextView reg;
 
+    @SuppressLint({"MissingInflatedId", "CutPasteId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        UsernameEditText = findViewById(R.id.username_edittext);
+        mAuth = FirebaseAuth.getInstance();
+        EmailEditText = findViewById(R.id.email_edittext);
         PasswordEditText = findViewById(R.id.password_edittext);
-        LoginButton = findViewById(R.id.login_button);
-        RegisterButton = findViewById(R.id.register_button);
-        LoginButton.setOnClickListener(new View.OnClickListener() {
+        Button loginButton = findViewById(R.id.login_button);
+        Button registerButton = findViewById(R.id.register_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Syarat login harus input username dan password (contoh diset default loginnya sebagai admin)
-                String username = UsernameEditText.getText().toString().trim();
-                String password = PasswordEditText.getText().toString().trim();
 
-                if (username.isEmpty() || password.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Mohon lengkapi semua field!", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(MainActivity.this, "Login Berhasil!", Toast.LENGTH_SHORT).show();
-                    // jika berhasil login, pindah ke halaman LoginSuccess
-                    Intent i = new Intent(MainActivity.this, LoginSuccessActivity.class);
-                    i.putExtra("username", username);
-                    startActivity(i);
-                }
+                cekLogin();
             }
         });
 
         // OnClickListener ketika tombol Register di klik dan akan berpindah ke halaman RegisterActivity
-        RegisterButton.setOnClickListener(new View.OnClickListener() {
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 showAlert();
             }
         });
-    }
-            private void showAlert() {
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("Alert");
-                builder.setMessage("Apakah anda yakin ingin membuat akun!?");
-                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        // Tambahkan logika yang diinginkan di sini
-                        Log.d("Alert", "Tombol OK diklik.");
-                        // Intent untuk membuka RegisterActivity
-                        Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
-                        startActivity(intent);
+
+        reg = findViewById(R.id.register_button);
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
             }
         });
-                AlertDialog dialog = builder.create();
-                dialog.show();
+    }
+
+    private void cekLogin() {
+        email = EmailEditText.getText().toString();
+        password = PasswordEditText.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(MainActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
+                            // jika berhasil login, pindah ke halaman LoginSuccess
+                            Intent i = new Intent(MainActivity.this, LoginSuccessActivity.class);
+                            i.putExtra("email", email);
+                            startActivity(i);
+                        } else {
+                            Toast.makeText(MainActivity.this, "Login Gagal, Email Atau Password Anda Salah!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+
+    private void showAlert() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Alert");
+        builder.setMessage("Apakah anda yakin ingin membuat akun!?");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Tambahkan logika yang diinginkan di sini
+                Log.d("Alert", "Tombol OK diklik.");
+                // Intent untuk membuka RegisterActivity
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
-
--- app/src/main/java/com/rama/myapplication2/RegisterActivity.java
+++ app/src/main/java/com/rama/myapplication2/RegisterActivity.java
@@ -1,57 +1,83 @@
 package com.rama.myapplication2;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class RegisterActivity extends AppCompatActivity {
 
     private EditText NameEditText, EmailEditText, PasswordEditText, ConfirmPasswordEditText;
-    private Button RegisterButton;
+    TextView log;
+    FirebaseAuth mAuth;
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+        log = findViewById(R.id.unregister_button);
 
-        // Inisialisasi views
+        // OnClickListener untuk tombol sudah punya akun
+        log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+            }
+        });
+
+        // Inisialisasi views pada saat melakukan input
         NameEditText = findViewById(R.id.name_edittext);
         EmailEditText = findViewById(R.id.email_edittext);
         PasswordEditText = findViewById(R.id.password_edittext);
         ConfirmPasswordEditText = findViewById(R.id.confirmpassword_edittext);
-        RegisterButton = findViewById(R.id.register_button);
+        Button registerButton = findViewById(R.id.register_button);
 
         // OnClickListener untuk tombol Register
-        RegisterButton.setOnClickListener(new View.OnClickListener() {
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // String untuk menginput text register
-                String name = NameEditText.getText().toString().trim();
-                String email = EmailEditText.getText().toString().trim();
-                String password = PasswordEditText.getText().toString().trim();
-                String confirmPassword = ConfirmPasswordEditText.getText().toString().trim();
-
-                if (TextUtils.isEmpty(name) || TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
-                    Toast.makeText(RegisterActivity.this, "Gagal register. Mohon lengkapi semua field.", Toast.LENGTH_SHORT).show();
-                } else if (!password.equals(confirmPassword)) {
-                    Toast.makeText(RegisterActivity.this, "Gagal register. Password dan konfirmasi password tidak sama.", Toast.LENGTH_SHORT).show();
-                } else {
-                    // Jika user berhasil register maka akan berpindah ke halaman MainActivity
-                    Toast.makeText(RegisterActivity.this, "Berhasil register.", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
+                performRegistration();
                 }
-            }
-        });
-    }
+            });}
 
+    // Deklarasi string untuk penginputan, fungsi, dan notifikasi pada saat register
+    public void performRegistration(){
+        String name = NameEditText.getText().toString();
+        String email = EmailEditText.getText().toString();
+        String password = PasswordEditText.getText().toString();
+        String confirmPassword = ConfirmPasswordEditText.getText().toString();
+
+        if (TextUtils.isEmpty(name) || TextUtils.isEmpty(email) || TextUtils.isEmpty(password) || TextUtils.isEmpty(confirmPassword)) {
+            // Jika ada kolom yang kosong
+            Toast.makeText(getApplicationContext(), "Register Failed! Please fill in all fields", Toast.LENGTH_SHORT).show();
+        } else if (!password.equals(confirmPassword)) {
+            // Jika password dan confirm password tidak sama
+            Toast.makeText(getApplicationContext(), "Register Failed! Password and Confirm Password are not the same", Toast.LENGTH_SHORT).show();
+        } else {
+            // Melakukan pendaftaran database dimasukan ke Firebase
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            // Jika pendaftaran berhasil
+                            Toast.makeText(getApplicationContext(), "Register Success, Come In!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            // Jika pendaftaran gagal karena penginputan tidak benar
+                            Toast.makeText(getApplicationContext(), "Registrasi Failed! Fill all field is correct!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+    }
 }
+
